449164e96a1fbb0ffabe8024677c5346048842e9
unknown <dayvdelb@gmail.com>
2023-09-26 00:13:51 -0300

    versao unica

---
+++ index.js
@@ -0,0 +1,53 @@
+let respost1 = window.document.getElementById('respost1');
+let respost2 = window.document.getElementById('respost2');
+let respost3 = window.document.getElementById('respost3');
+let respost4 = window.document.getElementById('respost4');
+let respost5 = window.document.getElementById('respost5');
+let img = window.document.getElementById('setaAcima');
+
+
+function girarImg1(){
+    if(setaAcima.style.transform === 'rotate(0deg)'){
+        setaAcima.style.transform = 'rotate(180deg)';
+        respost1.style.display = 'block';  
+    }else{
+        setaAcima.style.transform = 'rotate(0deg)';
+        respost1.style.display = 'none';
+    }
+}
+function girarImg2(){
+    if(setaAcima.style.transform === 'rotate(0deg)'){
+        setaAcima.style.transform = 'rotate(180deg)';
+        respost2.style.display = 'block';  
+    }else{
+        setaAcima.style.transform = 'rotate(0deg)';
+        respost2.style.display = 'none';
+    }
+}
+function girarImg3(){
+    if(setaAcima.style.transform === 'rotate(0deg)'){
+        setaAcima.style.transform = 'rotate(180deg)';
+        respost3.style.display = 'block';  
+    }else{
+        setaAcima.style.transform = 'rotate(0deg)';
+        respost3.style.display = 'none';
+    }
+}
+function girarImg4(){
+    if(setaAcima.style.transform === 'rotate(0deg)'){
+        setaAcima.style.transform = 'rotate(180deg)';
+        respost4.style.display = 'block';  
+    }else{
+        setaAcima.style.transform = 'rotate(0deg)';
+        respost4.style.display = 'none';
+    }
+}
+function girarImg5(){
+    if(setaAcima.style.transform === 'rotate(0deg)'){
+        setaAcima.style.transform = 'rotate(180deg)';
+        respost5.style.display = 'block';  
+    }else{
+        setaAcima.style.transform = 'rotate(0deg)';
+        respost5.style.display = 'none';
+    }
+}
